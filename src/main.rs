f180fbb369cdc11a47a6143c2f08d16d78066a40
Sting Alleman <stingalleman@icloud.com>
2023-10-28 23:21:36 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,58 @@
-use std::time::Duration;
+use futures::stream::StreamExt;
+use std::{env, io, str};
+use tokio_util::codec::{Decoder, Encoder};
 
-fn main() {
-    println!("running");
-    let mut port = serialport::new("/dev/ttyUSB0", 115_200)
-        .timeout(Duration::from_millis(10000))
-        .parity(serialport::Parity::Even)
-        .stop_bits(serialport::StopBits::One)
-        .data_bits(serialport::DataBits::Seven)
-        .open()
-        .expect("Failed to open port");
+use bytes::BytesMut;
+use tokio_serial::SerialPortBuilderExt;
 
-    let mut serial_buf: Vec<u8> = vec![0; 20480];
-    port.read(serial_buf.as_mut_slice())
-        .expect("Found no data!");
+#[cfg(unix)]
+const DEFAULT_TTY: &str = "/dev/ttyUSB0";
+#[cfg(windows)]
+const DEFAULT_TTY: &str = "COM1";
 
-    println!("{}", String::from_utf8(serial_buf).unwrap());
-    // println!("{:?}", serial_buf)
+struct LineCodec;
+
+impl Decoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        let newline = src.as_ref().iter().position(|b| *b == b'\n');
+        if let Some(n) = newline {
+            let line = src.split_to(n + 1);
+            return match str::from_utf8(line.as_ref()) {
+                Ok(s) => Ok(Some(s.to_string())),
+                Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid String")),
+            };
+        }
+        Ok(None)
+    }
+}
+
+impl Encoder<String> for LineCodec {
+    type Error = io::Error;
+
+    fn encode(&mut self, _item: String, _dst: &mut BytesMut) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> tokio_serial::Result<()> {
+    let mut args = env::args();
+    let tty_path = args.nth(1).unwrap_or_else(|| DEFAULT_TTY.into());
+
+    let mut port = tokio_serial::new(tty_path, 9600).open_native_async()?;
+
+    #[cfg(unix)]
+    port.set_exclusive(false)
+        .expect("Unable to set serial port exclusive to false");
+
+    let mut reader = LineCodec.framed(port);
+
+    while let Some(line_result) = reader.next().await {
+        let line = line_result.expect("Failed to read line");
+        println!("{}", line);
+    }
+    Ok(())
 }
