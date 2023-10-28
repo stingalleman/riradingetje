c2f501c9bc0fee94386c7b73adbb0571ec789e88
Sting Alleman <stingalleman@icloud.com>
2023-10-28 22:40:01 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::time::Duration;
+
 fn main() {
-    let ports = serialport::available_ports().expect("No ports found!");
-    for p in ports {
-        println!("{}", p.port_name);
-    }
+    let mut port = serialport::new("/dev/ttyUSB0", 115_200)
+        .timeout(Duration::from_millis(10))
+        .open()
+        .expect("Failed to open port");
+
+    let mut serial_buf: Vec<u8> = vec![0; 32];
+    port.read(serial_buf.as_mut_slice())
+        .expect("Found no data!");
 }
