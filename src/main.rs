a80dc699856bee54534b6c7808362ede5c3dc2d9
Sting Alleman <stingalleman@icloud.com>
2023-10-28 22:59:37 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 fn main() {
     println!("running");
     let mut port = serialport::new("/dev/ttyUSB0", 115_200)
-        .timeout(Duration::from_millis(10))
+        .timeout(Duration::from_millis(1000))
         .parity(serialport::Parity::Even)
         .stop_bits(serialport::StopBits::One)
         .data_bits(serialport::DataBits::Seven)
