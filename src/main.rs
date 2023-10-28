db11b2d2ec2ccd376f945982e8f4b29e94057d4e
Sting Alleman <stingalleman@icloud.com>
2023-10-28 23:04:14 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,6 @@ fn main() {
     port.read(serial_buf.as_mut_slice())
         .expect("Found no data!");
 
-    // println!("{}", String::from_utf8(serial_buf).unwrap());
-    println!("{:?}", serial_buf)
+    println!("{}", String::from_utf8(serial_buf).unwrap());
+    // println!("{:?}", serial_buf)
 }
