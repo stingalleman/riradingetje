42f4fb55636d7baa32d45c0193c3b24b43a492d9
Sting Alleman <stingalleman@icloud.com>
2023-10-28 22:53:21 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     println!("running");
     let mut port = serialport::new("/dev/ttyUSB0", 115_200)
         .timeout(Duration::from_millis(10))
+        .parity(serialport::Parity::Even)
+        .stop_bits(serialport::StopBits::One)
+        .data_bits(serialport::DataBits::Seven)
         .open()
         .expect("Failed to open port");
 
