f126c9d32d6428ec6c3494037d258891519c2edc
Sting Alleman <stingalleman@icloud.com>
2023-10-28 23:28:47 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,6 @@ use tokio_util::codec::{Decoder, Encoder};
 use bytes::BytesMut;
 use tokio_serial::SerialPortBuilderExt;
 
-#[cfg(unix)]
-const DEFAULT_TTY: &str = "/dev/ttyUSB0";
-#[cfg(windows)]
-const DEFAULT_TTY: &str = "COM1";
-
 struct LineCodec;
 
 impl Decoder for LineCodec {
@@ -19,7 +14,7 @@ impl Decoder for LineCodec {
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         let newline = src.as_ref().iter().position(|b| *b == b'\n');
         if let Some(n) = newline {
-            let line = src.split_to(n + 1);
+            let line = src.split_to(n + 0);
             return match str::from_utf8(line.as_ref()) {
                 Ok(s) => Ok(Some(s.to_string())),
                 Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid String")),
@@ -40,7 +35,7 @@ impl Encoder<String> for LineCodec {
 #[tokio::main]
 async fn main() -> tokio_serial::Result<()> {
     let mut args = env::args();
-    let tty_path = args.nth(1).unwrap_or_else(|| DEFAULT_TTY.into());
+    let tty_path = args.nth(1).unwrap_or_else(|| "/dev/ttyUSB0".into());
 
     let mut port = tokio_serial::new(tty_path, 115200)
         .data_bits(tokio_serial::DataBits::Seven)
