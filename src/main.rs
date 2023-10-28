721c8b999a77b2bd23013a17b90942d1b4683989
Sting Alleman <stingalleman@icloud.com>
2023-10-29 01:42:02 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,10 @@
-// use futures::stream::StreamExt;
-// use std::{ascii, env, io, str};
-// use tokio_util::codec::Decoder;
-
-// use bytes::BytesMut;
-// use tokio_serial::SerialPortBuilderExt;
-
-// #[derive(Debug)]
-// struct DSMR {
-//     version: u8,
-//     // YYMMDDhhmm, ssX
-//     timestamp: u64,
-//     delivered_1: f64,
-//     delivered_2: f64,
-//     tarief: u8,
-//     delivered: f64,
-//     voltage: f32,
-//     current: f32,
-//     instantaneous_power: f64,
-// }
-// struct LineCodec;
-
-// impl Decoder for LineCodec {
-//     type Item = String;
-//     type Error = io::Error;
-
-//     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-//         let newline = src.as_ref().iter().position(|b| *b == b'\n');
-//         if let Some(n) = newline {
-//             let line = src.split_to(n + 1);
-//             return match str::from_utf8(line.as_ref()) {
-//                 Ok(s) => Ok(Some(s.to_string())),
-//                 Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid String")),
-//             };
-//         }
-//         Ok(None)
-//     }
-// }
-
-// fn get_value(str: String) -> String {
-//     let start = str.find("(").unwrap() + 1;
-//     let end = str.find(")").unwrap();
-//     let res = &str[start..end];
-
-//     res.to_string()
-// }
-
-// #[tokio::main]
-// async fn main() -> tokio_serial::Result<()> {
-//     let mut args = env::args();
-//     let tty_path = args.nth(1).unwrap_or_else(|| "/dev/ttyUSB0".into());
-
-//     let mut port = tokio_serial::new(tty_path, 115200)
-//         .data_bits(tokio_serial::DataBits::Eight)
-//         .stop_bits(tokio_serial::StopBits::One)
-//         .parity(tokio_serial::Parity::None)
-//         .open_native_async()?;
-
-//     #[cfg(unix)]
-//     port.set_exclusive(false)
-//         .expect("Unable to set serial port exclusive to false");
-
-//     let mut reader = LineCodec.framed(port);
-
-//     let mut buf: DSMR = DSMR {
-//         version: 0,
-//         timestamp: 0,
-//         delivered_1: 0.0,
-//         delivered_2: 0.0,
-//         tarief: 0,
-//         delivered: 0.0,
-//         voltage: 0.0,
-//         current: 0.0,
-//         instantaneous_power: 0.0,
-//     };
-
-//     while let Some(line_result) = reader.next().await {
-//         let line = line_result.expect("Failed to read line");
-
-//         // if line.len() == 0 || line.contains(char::is_whitespace) {
-//         //     println!("whitespace, {}", line.contains(char::is_whitespace));
-//         //     continue;
-//         // }
-
-//         if line.contains("/") {
-//             buf = DSMR {
-//                 version: 0,
-//                 timestamp: 0,
-//                 delivered_1: 0.0,
-//                 delivered_2: 0.0,
-//                 tarief: 0,
-//                 delivered: 0.0,
-//                 voltage: 0.0,
-//                 current: 0.0,
-//                 instantaneous_power: 0.0,
-//             };
-//         } else if line.contains("!") {
-//             // println!("finished");
-//             println!("{:?}", buf);
-//             continue;
-//         }
-
-//         // println!("{} - {}", line, line.len());
-//         // let parameter = &line[0..line.find("(").unwrap()];
-//         // println!("{}", parameter);
-
-//         if line.contains("0-0:1.0.0") {
-//             let x = get_value(line).strip_suffix("S").unwrap().to_string();
-//             buf.timestamp = x.parse().unwrap();
-//         }
-//     }
-//     Ok(())
-// }
-
-use std::{io::Read, time::Duration};
+use std::{env, io::Read, time::Duration};
 
 fn main() {
-    let port = serialport::new("/dev/ttyUSB0", 115_200)
+    let mut args = env::args();
+    let tty_path = args.nth(1).unwrap_or_else(|| "/dev/ttyUSB0".into());
+
+    let port = serialport::new(tty_path, 115_200)
         .data_bits(serialport::DataBits::Eight)
         .stop_bits(serialport::StopBits::One)
         .parity(serialport::Parity::None)
@@ -130,6 +19,6 @@ fn main() {
         let telegram = x.to_telegram().unwrap();
         let state = dsmr5::Result::<dsmr5::state::State>::from(&telegram).unwrap();
 
-        println!("{}", state.power_delivered.unwrap());
+        println!("{:?}", state);
     }
 }
