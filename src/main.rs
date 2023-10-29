adc38f9f7aaa084cad61e5c091e23da983060005
Sting Alleman <stingalleman@icloud.com>
2023-10-29 02:50:38 +0200

    aa

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
+use std::time::Duration;
 
+use chrono::NaiveDate;
 use futures::prelude::*;
 use influxdb2::models::DataPoint;
 use influxdb2::Client;
+use std::io::Read;
 
 #[tokio::main]
 async fn main() {
@@ -11,30 +14,46 @@ async fn main() {
     let token = &args[2];
 
     println!("{} - {}", tty_path, token);
-    let bucket = "test";
+    let bucket = "test-meter";
     let client = Client::new("https://influxdb.stingalleman.dev", "lab", token);
 
-    let points = vec![DataPoint::builder("cpu")
-        .tag("host", "server01")
-        .field("usage", 0.5)
-        .build()
-        .unwrap()];
+    let port = serialport::new(tty_path, 115_200)
+        .data_bits(serialport::DataBits::Eight)
+        .stop_bits(serialport::StopBits::One)
+        .parity(serialport::Parity::None)
+        .timeout(Duration::from_millis(2000))
+        .open()
+        .expect("Failed to open port");
 
-    client.write(bucket, stream::iter(points)).await.unwrap();
+    let reader = dsmr5::Reader::new(port.bytes());
 
-    // let port = serialport::new(tty_path, 115_200)
-    //     .data_bits(serialport::DataBits::Eight)
-    //     .stop_bits(serialport::StopBits::One)
-    //     .parity(serialport::Parity::None)
-    //     .timeout(Duration::from_millis(2000))
-    //     .open()
-    //     .expect("Failed to open port");
+    for readout in reader {
+        let x = readout.unwrap();
+        let telegram = x.to_telegram().unwrap();
+        let state = dsmr5::Result::<dsmr5::state::State>::from(&telegram).unwrap();
 
-    // let reader = dsmr5::Reader::new(port.bytes());
+        let state_timestamp = state.datetime.unwrap();
+        let timestamp = NaiveDate::from_ymd_opt(
+            state_timestamp.year.into(),
+            state_timestamp.month.into(),
+            state_timestamp.day.into(),
+        )
+        .unwrap()
+        .and_hms_opt(
+            state_timestamp.hour.into(),
+            state_timestamp.minute.into(),
+            state_timestamp.second.into(),
+        )
+        .unwrap()
+        .timestamp();
 
-    // for readout in reader {
-    //     let x = readout.unwrap();
-    //     let telegram = x.to_telegram().unwrap();
-    //     let state = dsmr5::Result::<dsmr5::state::State>::from(&telegram).unwrap();
-    // }
+        let points: Vec<DataPoint> = vec![DataPoint::builder("meter")
+            // .tag("host", "server01")
+            .field("power_delivered", state.power_delivered.unwrap())
+            .timestamp(timestamp)
+            .build()
+            .unwrap()];
+
+        client.write(bucket, stream::iter(points)).await.unwrap();
+    }
 }
