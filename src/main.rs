fe891f0710261df3c1e1fa821b3e8c4680b5c99e
Sting Alleman <stingalleman@icloud.com>
2023-10-29 03:02:16 +0100

    aa

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ async fn main() {
             .unwrap();
         // .timestamp_nanos_opt()
         // .unwrap();
-        println!("{}", timestamp);
+        println!("{}", timestamp.timestamp());
 
         // let power_delivered = state.power_delivered.unwrap();
         // println!("{}", timestamp);
